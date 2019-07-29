96569c903437d89f24f913fde9b8712a50c0d4cf
aucd29 <aucd29@hanwha.com>
2019-07-30 08:17:46 +0900

    -

--- app/src/androidTest/java/com/example/imagebank/ui/fragment/TabChangeTest.kt
+++ app/src/androidTest/java/com/example/imagebank/ui/fragment/TabChangeTest.kt
@@ -20,7 +20,6 @@ import com.example.imagebank.util.withBackgroundColor
 import org.hamcrest.Matchers.allOf
 import org.junit.After
 import org.junit.Before
-import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
