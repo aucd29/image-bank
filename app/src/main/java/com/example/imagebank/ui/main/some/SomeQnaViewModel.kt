93d0d9e0555583a52721ad3daee7ee677dc7ccf6
aucd29 <aucd29@hanwha.com>
2019-07-31 11:01:32 +0900

    - 어린이날 기념 -_ - 디테일 살리기

--- app/src/main/java/com/example/imagebank/ui/main/some/SomeQnaViewModel.kt
+++ app/src/main/java/com/example/imagebank/ui/main/some/SomeQnaViewModel.kt
@@ -1,12 +1,20 @@
 package com.example.imagebank.ui.main.some
 
 import android.app.Application
+import android.view.View
+import androidx.core.view.ViewCompat
+import androidx.core.view.ViewPropertyAnimatorListener
 import androidx.databinding.ObservableField
-import androidx.databinding.ObservableInt
+import androidx.recyclerview.widget.RecyclerView
 import brigitte.RecyclerExpandableViewModel
-import brigitte.RecyclerViewModel
+import brigitte.ViewPropertyEndAnimatorListener
 import com.example.imagebank.R
 import com.example.imagebank.model.local.QnaItem
+import jp.wasabeef.recyclerview.animators.BaseItemAnimator
+import jp.wasabeef.recyclerview.animators.FadeInDownAnimator
+import jp.wasabeef.recyclerview.animators.FlipInBottomXAnimator
+import jp.wasabeef.recyclerview.animators.SlideInDownAnimator
+import jp.wasabeef.recyclerview.internal.ViewHelper
 import org.slf4j.LoggerFactory
 import javax.inject.Inject
 
@@ -23,6 +31,41 @@ class SomeQnaViewModel @Inject constructor(
 
     val horDecoration = ObservableField(R.drawable.shape_divider_gray)
     val verDecoration = ObservableField(R.drawable.shape_divider_gray)
+    val itemAnimator  = ObservableField<RecyclerView.ItemAnimator>(object: SlideInDownAnimator() {
+        override fun preAnimateAddImpl(holder: RecyclerView.ViewHolder) {
+            holder.itemView.apply {
+                alpha = 0f
+                translationY = height * -1f
+            }
+
+            ViewCompat.animate(holder.itemView)
+                .translationY(0f)
+                .alpha(1f)
+                .setDuration(moveDuration)
+                .setInterpolator(mInterpolator)
+                .setListener(object: ViewPropertyAnimatorListener {
+                    override fun onAnimationCancel(view: View?) {
+                        ViewHelper.clear(view)
+                    }
+
+                    override fun onAnimationStart(view: View?) {
+                        dispatchAddStarting(holder)
+                    }
+
+                    override fun onAnimationEnd(view: View?) {
+                        ViewHelper.clear(view)
+                        dispatchAddFinished(holder)
+                        mAddAnimations.remove(holder)
+
+                        if (!isRunning) {
+                            dispatchAnimationsFinished()
+                        }
+                    }
+                })
+                .start()
+        }
+        override fun animateAddImpl(holder: RecyclerView.ViewHolder?) { }
+    })
 
     init {
         initAdapter(R.layout.some_qna_parent_item, R.layout.some_qna_child_item)
